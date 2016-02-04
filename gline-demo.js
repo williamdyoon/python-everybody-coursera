591ff1e1f4539b65e5411073fc8ac81eaef501db
William Dongjun Yoon <williamdyoon@gmail.com>
2016-02-04 00:03:50 -0800

    4-using-databases-with-python

---
+++ gline-demo.js
@@ -0,0 +1,3 @@
+gline = [ ['Year','umich.edu','northwestern.edu','unl.edu','berkeley.edu','columbia.edu','virginia.edu','cam.ac.uk','gmail.com','hull.ac.uk','weber.edu'],
+['2005-12',42,12,12,9,8,8,7,7,7,6]
+];
